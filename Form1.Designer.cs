ca1b1eb913fbf64095802fb1b101b969664f854b
asty <104666282+astyxd@users.noreply.github.com>
2022-06-02 22:19:47 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PRJPesoIdeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnPesoIdeal = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(55, 74);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(379, 29);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.cmbSexo.Location = new System.Drawing.Point(264, 140);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(170, 32);
+            this.cmbSexo.TabIndex = 1;
+            this.cmbSexo.SelectedIndexChanged += new System.EventHandler(this.cmbSexo_SelectedIndexChanged);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(55, 143);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(156, 29);
+            this.txtAltura.TabIndex = 2;
+            // 
+            // btnPesoIdeal
+            // 
+            this.btnPesoIdeal.BackColor = System.Drawing.Color.Transparent;
+            this.btnPesoIdeal.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnPesoIdeal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnPesoIdeal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnPesoIdeal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesoIdeal.Location = new System.Drawing.Point(55, 227);
+            this.btnPesoIdeal.Name = "btnPesoIdeal";
+            this.btnPesoIdeal.Size = new System.Drawing.Size(212, 57);
+            this.btnPesoIdeal.TabIndex = 3;
+            this.btnPesoIdeal.Text = "Peso &Ideal";
+            this.btnPesoIdeal.UseVisualStyleBackColor = false;
+            this.btnPesoIdeal.Click += new System.EventHandler(this.btnPesoIdeal_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Transparent;
+            this.btnLimpar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnLimpar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnLimpar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Location = new System.Drawing.Point(55, 288);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(212, 57);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnSair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Location = new System.Drawing.Point(55, 348);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(212, 57);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(51, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(51, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Altura";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(260, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sexo";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PRJPesoIdeal.Properties.Resources.ideal;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(836, 600);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnPesoIdeal);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.cmbSexo);
+            this.Controls.Add(this.txtNome);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnPesoIdeal;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRJPesoIdeal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtAltura.Clear();
+            cmbSexo.Text = "";
+            txtNome.Focus();
+        }
+
+        private void cmbSexo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPesoIdeal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Altura = 0, PesoIdeal = 0;
+                Altura = double.Parse(txtAltura.Text);
+                if (cmbSexo.Text == "Feminino")
+                {
+                    PesoIdeal = (62.7 * Altura) - 44.7;
+                    MessageBox.Show(txtNome.Text + " seu peso ideal é: " + PesoIdeal.ToString() + " KG", "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (cmbSexo.Text == "Masculino")
+                {
+                    PesoIdeal = (72.7 * Altura) - 58;
+                    MessageBox.Show(txtNome.Text + " seu peso ideal é: " + PesoIdeal.ToString() + " KG", "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Escolha o sexo", "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Verifique os valores informados.", "Peso Ideal",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
